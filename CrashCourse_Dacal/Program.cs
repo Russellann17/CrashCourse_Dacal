15662e5622c6cb1e8f6686214de92aa3ef66b5f6
Russellann17 <dacalrussell@gmail.com>
2024-09-17 20:24:41 +0800

    Changes for lesson 9 applying the lesson 10

--- CrashCourse_Dacal/Program.cs
+++ CrashCourse_Dacal/Program.cs
@@ -16,20 +16,17 @@ namespace CrashCourse_Dacal
 
             while (exit)
             { 
-                Console.WriteLine("[1] Addition" +
-                              "\n[2] Subtraction" +
-                              "\n[3] Multiplication" +
-                              "\n[4] Division" +
-                              "\n[5] Lesson 1 : Hello World" +
-                              "\n[6] Lesson 2 : Variables" +
-                              "\n[7] Lesson 3 : Input & Output" +
-                              "\n[8] Lesson 4 : Operators & Expressions" +
-                              "\n[9] Lesson 5 : Conditional Statements" +
-                              "\n[10] Lesson 6 : Switch Statements" +
-                              "\n[11] Lesson 7a: While Loop" +
-                              "\n[12] Lesson 7b: For Loop" +
-                              "\n[13] Lesson 8 : Arrays" +
-                              "\n[0] Exit");
+                Console.WriteLine("[1] Lesson 1 : Hello World" +
+                                  "\n[2] Lesson 2 : Variables" +
+                                  "\n[3] Lesson 3 : Input & Output" +
+                                  "\n[4] Lesson 4 : Operators & Expressions" +
+                                  "\n[5] Lesson 5 : Conditional Statements" +
+                                  "\n[6] Lesson 6 : Switch Statements" +
+                                  "\n[7] Lesson 7a: While Loop" +
+                                  "\n[8] Lesson 7b: For Loop" +
+                                  "\n[9] Lesson 8 : Arrays" +
+                                  "\n[10] Lesson 9 : Functions" +
+                                  "\n[0] Exit");
 
                 Console.Write("Choose: ");
                 choice = Convert.ToInt32(Console.ReadLine());
@@ -42,123 +39,48 @@ namespace CrashCourse_Dacal
 
                 switch (choice)
                 {
-                    case 1: //Addition
-                        Console.Write("How many numbers do you want to add? ");
-                        numberOfInputs = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Enter " + numberOfInputs + " numbers: ");
-                        
-                        int[] numbers = new int[numberOfInputs];
-                        for (int i = 0; i < numbers.Length; i++)
-                        {
-                            numbers[i] = Convert.ToInt32(Console.ReadLine());
-                        }
-                        Console.WriteLine("The Sum of all numbers is" + add(numbers) + ".\n\n");
-                        break;
-
-                    case 2: //Subtraction
-                        Console.Write("1st Number: ");
-                        num1 = Convert.ToInt32(Console.ReadLine());
-                        Console.Write("2nd Number: ");
-                        num2 = Convert.ToInt32(Console.ReadLine());
-
-                        Console.WriteLine("The difference of two numbers is "+ subtract(num1, num2) + ".\n\n");
-                        break;
-
-                    case 3: //Multiplication
-                        Console.Write("How many numbers do you want to multiply? ");
-                        numberOfInputs = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Enter "+ numberOfInputs +" numbers: ");
-                        int[] number = new int[numberOfInputs];
-                        for (int i = 0; i < number.Length; i++)
-                        {
-
-                            number[i] = Convert.ToInt32(Console.ReadLine());
-                        }
-                        Console.WriteLine("The product of all numbers is " + multiply(number) + ".\n\n");
-                        break;
-
-                    case 4: //Division
-                        Console.Write("1st Number: ");
-                        fnum1 = Convert.ToSingle(Console.ReadLine());
-                        Console.Write("2nd Number: ");
-                        fnum2 = Convert.ToSingle(Console.ReadLine());
-
-                        Console.WriteLine("The quotient of two numbers is " + divide(fnum1, fnum2) + ".\n\n");
-                        break;
-                    case 5:
+                    case 1:
                         CrashCodeClasses.Functions.Lesson1();
                         Console.WriteLine("\n");
                         break;
-                    case 6:
+                    case 2:
                         CrashCodeClasses.Functions.Lesson2();
                         Console.WriteLine("\n");
                         break;
-                    case 7:
+                    case 3:
                         CrashCodeClasses.Functions.Lesson3();
                         Console.WriteLine("\n");
                         break;
-                    case 8:
+                    case 4:
                         CrashCodeClasses.Functions.Lesson4();
                         Console.WriteLine("\n");
                         break;
-                    case 9:
+                    case 5:
                         CrashCodeClasses.Functions.Lesson5();
                         Console.WriteLine("\n");
                         break;
-                    case 10:
+                    case 6:
                         CrashCodeClasses.Functions.Lesson6();
                         Console.WriteLine("\n");
                         break;
-                    case 11:
+                    case 7:
                         CrashCodeClasses.Functions.Lesson7a();
                         Console.WriteLine("\n");
                         break;
-                    case 12:
+                    case 8:
                         CrashCodeClasses.Functions.Lesson7b();
                         Console.WriteLine("\n");
                         break;
-                    case 13:
+                    case 9:
                         CrashCodeClasses.Functions.Lesson8();
+                        Console.WriteLine("\n");
+                        break;
+                    case 10:
+                        Lesson9.L9_functions();
                         Console.WriteLine("\n");
                         break;
                 }
             }
         }
-
-        //Addition
-        static int add(int[] numbers)
-        {
-            int sum = 0;
-
-            for(int i = 0; i< numbers.Length; i++)
-            {
-                sum += numbers[i];
-            }
-            return sum;
-        }
-        //Subtraction
-        static int subtract(int num1, int num2 )
-        {
-            return (num1 - num2);
-        }
-
-        //Multiplication
-        static int multiply(int[] number) 
-        {
-            int product = 1;
-
-            for (int i = 0; i < number.Length; i++)
-            {
-                product *= number[i];
-            }
-            return product;
-        }
-
-        //Division
-        static float divide(float num1, float num2) 
-        {
-            return num1 / num2;
-        }
-
     }
 }
