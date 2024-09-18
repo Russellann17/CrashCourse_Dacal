bd8c644546670358ba4c8515bf6630bca9e508cc
Russellann17 <dacalrussell@gmail.com>
2024-09-18 10:09:04 +0800

    Lesson 10 - Object Methods

---
+++ CrashCourse_Dacal/L10_Student.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrashCourse_Dacal
+{
+    internal class L10_Student
+    {
+        public string firstName;
+        public string lastName;
+        public int year;
+        public string course;
+        public string section;
+        public float midtermGrade;
+        public float finalGrade;
+
+        public L10_Student(string firstName, string lastName, int year, string course, string section, float midtermGrade, float finalGrade)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.year = year;
+            this.course = course;
+            this.section = section;
+            this.midtermGrade = midtermGrade;
+            this.finalGrade = finalGrade;
+        }
+
+        public void introduceSelf()
+        {
+            Console.WriteLine("\nSTUDENT INFORMATION:\n");
+            Console.WriteLine("Name           : " + firstName + " " + lastName);
+            Console.WriteLine("Course         : " + course);
+            Console.WriteLine("Year & Section : " + year + " - " + section);
+        }
+
+        public void evaluateGrade()
+        {
+            float ave = (midtermGrade + finalGrade) / 2;
+            Console.WriteLine("Average        : " + ave);
+
+            if (ave > 100)       Console.WriteLine("\nINVALID GRADE!\n");
+            else if (ave >= 98)  Console.WriteLine("\nCONGRATULATIONS! You are With Highest Honor.\n");
+            else if (ave >= 95)  Console.WriteLine("\nCONGRATULATIONS! You are With High Honor.\n");
+            else if (ave >= 90)  Console.WriteLine("\nCONGRATULATIONS! You are With Honor.\n");
+            else if (ave >= 75)  Console.WriteLine("\nCONGRATULATIONS! You passed.\n");
+            else                 Console.WriteLine("\nSorry but you failed.\n");
+        }
+    }
+}
--- CrashCourse_Dacal/Program.cs
+++ CrashCourse_Dacal/Program.cs
@@ -25,7 +25,8 @@ namespace CrashCourse_Dacal
                                   "\n[8] Lesson 7b: For Loop" +
                                   "\n[9] Lesson 8 : Arrays" +
                                   "\n[10] Lesson 9 : Functions" +
-                                  "\n[11] Lesson 10 : Constructor" +
+                                  "\n[11] Lesson 10a : Constructor" +
+                                  "\n[12] Lesson 10b : Object Methods" +
                                   "\n[0] Exit");
 
                 Console.Write("Choose: ");
@@ -80,14 +81,36 @@ namespace CrashCourse_Dacal
                         Console.WriteLine("\n");
                         break;
                     case 11:
-                        Console.WriteLine("Enter the details of the car: ");
+                        Console.WriteLine("\n\nEnter the details of the car: ");
                         Console.Write("Brand : ");
                         string brand = Console.ReadLine();
                         Console.Write("Model : ");
                         string model = Console.ReadLine();
                         Console.Write("Year  : ");
                         int year = Convert.ToInt32(Console.ReadLine());
+                        
                         Lesson10 car = new Lesson10(brand,model,year);
+                        break;
+                    case 12:
+                        Console.WriteLine("\n\nWelcome student! Please enter the fields below. ");
+                        Console.Write("\nFirst Name     : ");
+                        string firstName = Console.ReadLine();
+                        Console.Write("Last Name      : ");
+                        string lastName = Console.ReadLine();
+                        Console.Write("Year           : ");
+                        int yearlvl = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Course         : ");
+                        string course = Console.ReadLine();
+                        Console.Write("Section        : ");
+                        string section = Console.ReadLine();
+                        Console.Write("Midterm Grade  : ");
+                        float midtermGrade = Convert.ToSingle(Console.ReadLine());
+                        Console.Write("Final Grade    : ");
+                        float finalGrade = Convert.ToSingle(Console.ReadLine());
+                  
+                        L10_Student s = new L10_Student(firstName, lastName, yearlvl, course, section, midtermGrade, finalGrade);
+                        s.introduceSelf();
+                        s.evaluateGrade();
                         break;
                 }
             }
